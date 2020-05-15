ab97bc2ea7b284533dbb3a22b4abc6a2a5ef0ce6
sabrina-figueroa <62968239+sabrina-figueroa@users.noreply.github.com>
2020-05-15 12:33:49 -0700

    Update lung.m

--- lung.m
+++ lung.m
@@ -1,7 +1,13 @@
 %filename: lung.m (main program)
+%randomized model of oxygen transport inside the lungs 
+%this is to run without the pulmonary shunt with pneumonia 
 clear all
 clf
 global Pstar cstar n maxcount M Q camax RT cI;
 setup_lung
-cvsolve
-outchecklung
+cvsolve;
+outchecklung1
+shunt_frac=((cAbar)-cabar)/(cAbar-cI); %in this case we use sum(cA) because cA is the concentration of 
+                                      %oxygen per alveoli (100) in this
+                                      %case
+%for a normal case shunt fraction should be around 20% 
