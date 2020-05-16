905523dae609816dc55b9a1a0dfba256c94106b6
sabrina-figueroa <62968239+sabrina-figueroa@users.noreply.github.com>
2020-05-16 07:14:02 -0700

    Update setup_lung.m

--- setup_lung.m
+++ setup_lung.m
@@ -2,7 +2,7 @@
 %heterogeneity parameter (0<=beta<=1):
 %beta=0 for homogenous lung
 %beta=1 for no ventilation/perfusion correlation
-global VAtotal
+global Qtotal
 beta=0.5; %ventilation perfusion relationship whether or not one is independent of the other 
 %
 %number of iterations used in bisection:
@@ -53,18 +53,18 @@ VAtotal=5.0 ;    %(liters/minute)
 Qtotal=5.6  ;    %(liters/minute)
 %
 %expected alveolar ventilation per alveolus:
-m=15;
-%degree of 0.6 is normal ventilation below that is lack of ventilation and
-%above that is overventilation 
-deg=0.6;
-VAbar_mean=VAbarstar(n,m,deg) ;
-VAbar=sum(VAbar_mean)/(n);
+VAbar=VAtotal/n 
 %
 %expected perfusion per alveolus: 
 %m is the number of alveoli affected within 1:n 
 %deg is the degree of the pulmonary shunt from 0-1 : 1 is largest pulm
 %shunt. 
-Qbar=Qtotal/n;
+m=15;
+%degree of 0.6 is normal ventilation below that is lack of ventilation and
+%above that is overventilation 
+deg=0.6;
+Qbar_mean=Qbarstar(n,m,deg) ;
+Qbar=sum(Qbar_mean)/(n);
 %
 %random initialization 
 %of ventilation and perfusion:
