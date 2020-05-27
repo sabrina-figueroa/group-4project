ea2dd57f14db5ec2b070f4b69ef893648df111c2
sabrina-figueroa <62968239+sabrina-figueroa@users.noreply.github.com>
2020-05-26 18:06:06 -0700

    Update H.m

--- H.m
+++ H.m
@@ -1,5 +1,8 @@
+%increase alveolar partial pressure to reduce oxygen volume 
 function P=H(ca)
-%filename: H.m
-global Pstar cstar;
+global Pstar Cstar
 ratio=ca/cstar;
-P=Pstar*(ratio./(1-ratio)).^(1/3);
+F_inflamation=2.5 %force of inflamation vary parameter 
+F_fluid=1; %force due to fluid build-up 
+P=Pstar*(ratio./(1-ratio)).^(1/3)*(F_inflamation+F_fluid);
+end
